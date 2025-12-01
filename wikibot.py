e8783315d33a143a16d8fbe1dcd1ba496827dc38
pavani-tvs88 <76645064+pavani-tvs88@users.noreply.github.com>
2025-12-01 11:26:55 +0000

    Modified file wikibot

--- wikibot.py
+++ wikibot.py
@@ -2,15 +2,6 @@ import wikipedia
 wikipedia.set_lang("en")
 
 def scrape(name, length=3):
-    try:
-        summary = wikipedia.summary(name, sentences=length)
-        return summary
-    except wikipedia.DisambiguationError as e:
-        return f"Disambiguation error: {e.options}"
-    except wikipedia.PageError:
-        return "Page not found."
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
-result = wikipedia.summary("Python (programming language)") 
-print(result)
+    summary = wikipedia.summary(name, sentences=length)
+    return summary
 print(scrape("Microsoft"))
